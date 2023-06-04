1ee65a1cbaac2ee8acb32f2a31aa212d0223f503
ZachSellman <zachsellman21@gmail.com>
2023-06-04 14:33:49 -0500

    created basic home page and added handler in main.go for it

--- main.go
+++ main.go
@@ -16,12 +16,22 @@ var ADDRESS = fmt.Sprintf("%s:%s", HOST, PORT)
 func main() {
 	fmt.Println("serving on address: ", ADDRESS)
 	router := gin.Default()
+	router.LoadHTMLFiles("front-end/basic-home.html")
+
 	router.GET("/health", getHealth)
+	router.GET("/", getHomepage)
+
 	http.ListenAndServe(ADDRESS, router)
 }
 
 func getHealth(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"status": "serving",
+	})
+}
+
+func getHomepage(c *gin.Context) {
+	c.HTML(http.StatusOK, "basic-home.html", gin.H{
+		"title": "Main website",
 	})
 }
